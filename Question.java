21181a28fbf3b35298521a2333bf4d80a042af25
mareveles <mareveles1@buffs.wtamu.edu>
2018-01-28 21:18:39 -0600

    correction

---
+++ Question.java
@@ -0,0 +1,32 @@
+package com.spring2018.cidm4385.mareveles.geoquiz;
+
+/**
+ * Created by Miguel on 1/28/2018.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
